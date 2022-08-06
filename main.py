f2a29b49da1e8e1d7e25774fff5a1057e07d6c6b
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-06 07:10:46 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+First setup init 
+Self.bitcoin
+Self.ethereum
+Self.ada
+Self.cardano
+self.doge
+
+(Each currency is a parameter), (given amount is a parameter) 
+function: givenBitcoin, givenEth,givenDoge,givenADA,givenCardano
+Use ticker for each crypto parameter
+
+Bitcoin (based on given input) * given amount / ethereum (based on given input)
+
+Bitcoin (based on given input) * given amount/ ADA (based on given input)
+
+Bitcoin (based on given input) * given amount/ 
+Cardano (based on given input) 
+
+Bitcoin (based on given input) * given amount /
+Doge (based on given input)
+
+Ethereum (based on given input) * given amount / bitcoin 
+
+Bitcoin (based on given input) * given amount 
